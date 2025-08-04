eab5a51ab956121a34acc6dd49cdcf9cad5dd683
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-08-04 23:50:14 +0800

    1182

--- src/main/java/me/kall/entityejector/EntityEjector.java
+++ src/main/java/me/kall/entityejector/EntityEjector.java
@@ -7,9 +7,10 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -17,7 +18,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -27,14 +27,14 @@ public final class EntityEjector {
     public static final String MOD_NAME = "EntityEjector";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
-    public EntityEjector(@NotNull FMLJavaModLoadingContext context) {
-        IEventBus modBus = context.getModEventBus();
-        MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGHEST, (EntityJoinLevelEvent event) -> {
+    public EntityEjector() {
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGHEST, (EntityJoinWorldEvent event) -> {
             if (((IEntityType)event.getEntity().getType()).entityEjector$ejected()) event.setCanceled(true);
         });
         modBus.addListener((FMLCommonSetupEvent event) -> event.enqueueWork(() -> {
             for (String name : ENTITIES.get()) {
-                EntityType<?> entityType = ForgeRegistries.ENTITY_TYPES.getValue(ResourceLocation.parse(name));
+                EntityType<?> entityType = ForgeRegistries.ENTITIES.getValue(ResourceLocation.parse(name));
                 if (entityType != null) {
                     ((IEntityType)entityType).entityEjector$setEjected(true);
                 } else {
@@ -42,7 +42,7 @@ public final class EntityEjector {
                 }
             }
         }));
-        context.registerConfig(ModConfig.Type.COMMON, CONFIG);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CONFIG);
     }
 
     public static final ForgeConfigSpec CONFIG;
